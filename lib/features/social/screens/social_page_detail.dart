6b8f1a4944f17487d74fe7cf05614569921c0a2d
Huy Phung <phunghuyk24@gmail.com>
2025-11-24 16:03:16 +0700

    dady huy

--- lib/features/social/screens/social_page_detail.dart
+++ lib/features/social/screens/social_page_detail.dart
@@ -1,3 +1,4 @@
+//G:\flutter-app\lib\features\social\screens\social_page_detail.dart
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
