dc9d5ad5885bdc97fa47ae6ef47635a3ffc81499
apple <apple@apples-MacBook-Pro.local>
2025-12-25 10:38:01 +0700

    zegoClound call social v5: offline call (Android App killed)-v10 add class im.zego...

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,5 +1,16 @@
 allprojects {
     repositories {
+        // ZEGOCLOUD maven (chứa zpns + plugin push)
+        maven(url = "https://storage.zego.im/maven")
+        // Huawei/Heytap repo để tránh lỗi thiếu dependencies khi bật push vendor khác
+        maven(url = "https://developer.huawei.com/repo/")
+        maven {
+            url = uri("https://maven.columbus.heytapmobi.com/repository/releases/")
+            credentials {
+                username = "nexus"
+                password = "c0b08da17e3ec36c3870fed674a0bcb36abc2e23"
+            }
+        }
         google()
         mavenCentral()
     }
